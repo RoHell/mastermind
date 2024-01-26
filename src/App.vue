011fa10a382daea29f404d48052aa1335abca449
Karol Mąkowski <karol.makowski@cloudflight.io>
2024-01-26 19:45:16 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import TopBar from './components/TopBar.vue'
+</script>
+
+<template>
+  <header>
+    <TopBar />
+  </header>
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: var(--top-bar-height);
+  width: 100%;
+  background-color: gray;
+  padding: 0 2rem;
+  box-sizing: border-box;
+}
+
+main {
+  position: relative;
+  top: var(--top-bar-height);
+  height: calc(100% - var(--top-bar-height));
+  width: 100%;
+  padding: 2rem;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="top-bar">
+    <span class="top-bar__logo">mastermind</span>
+  </div>
+</template>
+
+<style scoped>
+.top-bar {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  max-width: var(--max-width);
+  margin: 0 auto;
+}
+
+.top-bar__logo {
+  font-weight: 700;
+}
+</style>
