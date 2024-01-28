83f7097ec303004e28ab6b137bcb3769b82e2445
Karol Mąkowski <karol.makowski@cloudflight.io>
2024-01-29 00:28:54 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
     })
   ],
 })
