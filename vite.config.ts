d47dd668a41e4d60c6de7b1564f3de9f0be53e94
Karol Mąkowski <karol.makowski@cloudflight.io>
2024-01-28 23:26:38 +0100

    add build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mastermind/',
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+    })
   ],
 })
