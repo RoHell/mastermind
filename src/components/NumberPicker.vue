24a4e35e01430b9b95395eaf4b615a8daf15a276
Karol Mąkowski <karol.makowski@cloudflight.io>
2024-01-29 20:28:15 +0100

    fix

--- src/components/NumberPicker.vue
+++ src/components/NumberPicker.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Ref, computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useNumbers } from '../composables'
 
 import NumberWrapper from './NumberWrapper.vue'
